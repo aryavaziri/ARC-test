dfc2500ed67612b77380ae93073b969769dad867
Arya <aryav@abhmfg.com>
2025-04-15 07:41:33 +0200

    sandbox Update

--- src/app/[tab]/[layout]/page.tsx
+++ src/app/[tab]/[layout]/page.tsx
@@ -1,11 +1,19 @@
-import LayoutRenderer from "@/components/Layout/LayoutRenderer";
+'use client';
 
-interface Props {
-    params: { tab: string; layout: string };
-}
+import { useParams, useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
 
-export default function DynamicLayoutPage({ params }: Props) {
-    return (
-        <LayoutRenderer tab={params.tab} layout={params.layout} />
-    );
+export default function DynamicLayoutPage() {
+    const params = useParams();
+    const router = useRouter();
+
+    const layoutId = useSelector((state: RootState) => {
+        const tab = state.tabs.tabs.find(t => t.label.toLowerCase() === params.tab.toString().toLowerCase());
+        const layout = tab?.layouts.find(l => l.route === params.layout);
+        return layout?.layoutId ?? null;
+    });
+
+    if (!layoutId) return <div>Layout not found</div>;
+    router.push(`/render?layoutId=${layoutId}`);
 }
